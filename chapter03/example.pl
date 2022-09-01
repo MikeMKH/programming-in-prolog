ed8a766f23af5293df96195c49f5f66f19a48dda
Mike Harris <MikeMKH@users.noreply.github.com>
2022-09-01 06:21:17 -0500

    add 3.6

--- chapter03/example.pl
+++ chapter03/example.pl
@@ -19,3 +19,29 @@ exp34_2 :-
   write(S),write('->'),
   alter(S, Z),
   write(Z),nl,!.
+
+% 3.6 joining structures together
+partsof(X, [X]) :- basicpart(X).
+partsof(X, P) :-
+  assembly(X, Subparts),
+  partsoflist(Subparts, P).
+
+partsoflist([], []).
+partsoflist([P|Tail], All) :-
+  partsof(P, Heahparts),
+  partsoflist(Tail, Tailparts),
+  append(Heahparts, Tailparts, All).
+
+assembly(sentence, [noun_phrase, verb_phrase]).
+assembly(noun_phrase, [determiner, noun]).
+assembly(verb_phrase, [verb, noun]).
+
+assembly(determiner, [the]).
+assembly(noun, [apple]).
+assembly(noun, [fruit]).
+assembly(verb, [is]).
+
+basicpart(apple).
+basicpart(fruit).
+
+% not sure how to use this
