3298ba62d19b541cd15c18a0c71b9d2bcb154a5b
Mike Harris <MikeMKH@users.noreply.github.com>
2022-11-15 05:54:34 -0600

    add 7.11
    awesome

--- chapter07/example.pl
+++ chapter07/example.pl
@@ -613,3 +613,32 @@ pythag(X, Y, Z, N) :-
 % Y = 6,
 % Z = 10 ;
 % false.
+
+% 7.11 symbolic differentiation
+
+?- op(300, yfx, ^).
+d(X, X, 1) :- !.
+d(C, _, 0) :- atomic(C).
+d(-U, X, -A) :- d(U, X, A).
+d(U+V, X, A+B) :- d(U, X, A), d(V, X, B).
+d(U-V, X, A-B) :- d(U, X, A), d(V, X, B).
+d(C*U, X, C*A) :- atomic(C), \+ C = X, d(U, X, A), !.
+d(U*V, X, B*U+A*V) :- d(U, X, A), d(V, X, B).
+d(U/V, X, A) :- d(U*V^(-1), X, A).
+d(U^C, X, C*U^(C-1)*W) :- atomic(C), \+ C = X, d(U, X, W).
+d(log(U), X, A*U^(-1)) :- d(U, X, A).
+
+% ?- d(x+1, x, R).
+% R = 1+0.
+
+% ?- d(x*x-2, x, R).
+% R = 1*x+1*x-0.
+
+% ?- d(x*y, x, R).
+% R = 0*x+1*y.
+
+% ?- d(x^2, x, R).
+% R = 2*x^(2-1)*1.
+
+% ?- d(5*x+x^2, x, R).
+% R = 5*1+2*x^(2-1)*1.
